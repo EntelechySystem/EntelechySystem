4ef7338544a71b1f56a0711ee35e7af26873885b
YeeCube <EthanSystem@outlook.com>
2024-05-22 22:41:45 +0800

    init 2024-05-22

---
+++ EntelechySystem_python/Experiments/CIS_010.py
@@ -0,0 +1,25 @@
+"""
+CIS 010 实验主程序
+"""
+
+from engine.simulator.simulator import simulator
+
+
+def main():
+    # %% 配置项
+    config = dict()
+    config['folderpath_config'] = r"Libraries/ConfigsLibrary/config_test"  # 配置项文件夹路径
+    config['foldername_engine'] = r"Engine"  # 引擎所在工程文件夹名称
+    config['folderpath_relpath_engine'] = r"../"  # 引擎所在工程文件夹相对本实验项目根路径文件夹之相对路径
+    config['folderpath_relpath_outputData'] = r"../"  # 输出数据所在工程文件夹相对本实验项目根路径文件夹之相对路径
+    config['is_auto_confirmation'] = True  # 是否自动确认一些比较危险的操作例如删除、移动、复制文件等。默认 False；
+
+    # %% 运行模拟器
+    simulator(config)
+
+    pass  # function
+
+
+if __name__ == '__main__':
+    main()
+
