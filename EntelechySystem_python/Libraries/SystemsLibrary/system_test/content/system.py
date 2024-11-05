b3357aae79d1fb8f78dc2f0fd897df51e0b9f7af
Yee Cube <EthanSystem@outlook.com>
2024-11-05 17:55:09 +0800

    dev 尝试导入临时的先验概念

--- EntelechySystem_python/Libraries/SystemsLibrary/system_test/content/system.py
+++ EntelechySystem_python/Libraries/SystemsLibrary/system_test/content/system.py
@@ -1,15 +1,17 @@
 """
 测试用的系统
 """
-
+from engine.externals import Path, pd
 import logging
 # from ComplexIntelligenceSystem_python.Core.define_units import define_neurons_units, define_op_units
 from ComplexIntelligenceSystem_python.Core.define_units import NeuronsUnits, NeuronsUnits_ForHumanRead, OperationUnits
 from ComplexIntelligenceSystem_python.Core.tools import Tools
 from ComplexIntelligenceSystem_python.Core.settings import Settings
-# from AgentsWorldSystem_python.Libraries.WorldLibrary.中国象棋_test.world_environment import *
-import logging
+# from AgentsWorldSystem_python.Libraries.WorldLibrary.中国象棋.world_environment import *
+# from AgentsWorldSystem_python.Libraries.WorldLibrary.烧水倒水.world_environment import *
 
+from ComplexIntelligenceSystem_python.Libraries.ModelsLibrary.model_test.model import Model
+from ElementalConceptionSystem_python.Libararies.ConceptionLibrary.conception_test.conception import Conception
 
 
 def system(para: dict, gb: dict):
@@ -19,6 +21,10 @@ def system(para: dict, gb: dict):
     ## #NOW 导入临时的简单的世界环境
 
     ## #NOW 导入临时的简单的先验知识
+    ### 初始化状态下，直接先预置一些概念
+    folderpath_conceptions_data = Path(gb['folderpath_data'] / 'conceptions')
+    df_基础概念_现代汉语字符库 = pd.read_pickle(folderpath_conceptions_data / '基础概念_现代汉语字符库.pkl')
+
 
     ## #NOW 开始交互式学习
     gb['is_interactive_learning'] = True
