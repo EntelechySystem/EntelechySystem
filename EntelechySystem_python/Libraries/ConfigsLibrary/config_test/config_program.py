43456e0a036d72d6261c29ce50bd3fb1e0778b2a
Yee Cube <EthanSystem@outlook.com>
2024-11-05 16:30:45 +0800

    dev 2024-11-05 16:30:31

--- EntelechySystem_python/Libraries/ConfigsLibrary/config_test/config_program.py
+++ EntelechySystem_python/Libraries/ConfigsLibrary/config_test/config_program.py
@@ -8,18 +8,26 @@ from engine.tools.Tools import Tools
 
 
 def main():
+    # %% 配置项
+    config = dict(
+        is_use_xlsx_as_config_file=False,  # 是否使用 Excel 文件作为配置文件。默认 True；
+    )
 
-    # 导入配置文件，从 Excel 文件
-    config = DataManageTools.load_configs_from_excel_file_then_save_as_python_file(Path(folderpath_config))
-    # config = DataManageTools.load_configs_from_excel_to_dict(Path(r'.'), True)
-
+    # %% 导入配置文件
+    if config['is_use_xlsx_as_config_file']:
+        # 导入配置文件，从 Excel 文件
+        config = DataManageTools.load_configs_from_excel_file_then_save_as_python_file(Path(folderpath_config))
+        config.update(config['is_use_xlsx_as_config_file'])
+    else:
+        # 导入配置文件，从 Python 文件
+        config = DataManageTools.load_configs_from_python_file_then_save_as_excel_file(Path(folderpath_config, 'config.py'))
+        pass  # if
     # 保存为 PKL 文件
     with open(Path(folderpath_config, r"config.pkl"), 'wb') as f:
         pickle.dump(config, f)
 
     print("运行完毕！")
     pass  # function
-
 
 
 if __name__ == '__main__':
@@ -36,5 +44,3 @@ if __name__ == '__main__':
         pass  # if
     folderpath_config = folderpath
     main()
-
-
