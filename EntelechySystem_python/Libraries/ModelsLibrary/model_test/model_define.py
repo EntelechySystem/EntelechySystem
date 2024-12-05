fba138aba9fe7720c562cdb9b201669044efae1c
Yee Cube <EthanSystem@outlook.com>
2024-12-05 12:19:13 +0800

    dev CIS
    定义 PyTorch 版本的模型运作单元；

--- EntelechySystem_python/Libraries/ModelsLibrary/model_test/model_define.py
+++ EntelechySystem_python/Libraries/ModelsLibrary/model_test/model_define.py
@@ -8,6 +8,12 @@ import numpy as np
 
 from engine.functions.ComplexIntelligenceSystem.Core.tools import Tools
 from .model_settings import ModelSettings
+import torch
+import jax.numpy as jnp
+from engine.functions.ComplexIntelligenceSystem.Core.tools import Tools
+from .model_settings import ModelSettings
+from dataclasses import dataclass
+
 
 # class InitUnits():
 #     """
@@ -22,9 +28,6 @@ from .model_settings import ModelSettings
 #         pass  # function
 #
 #     pass  # class
-
-
-import torch
 
 
 @dataclass
@@ -85,7 +88,6 @@ class ModelDefine():
                 max_num_links:
             """
             self.gid = torch.arange(N_units, dtype=torch.int64)
-            # self.uid = torch.arange(N_units, dtype=torch.uint64)
             self.units_name = np.array([Tools.generate_unique_identifier() for i in range(N_units)], np.dtype('S32'))
             self.units_type = torch.from_numpy(np.full(N_units, ModelSettings.dict_written_type_of_Units['neuron']))
             pass  # function
@@ -158,47 +160,91 @@ class ModelDefine():
     #
     #     pass  # class
 
-    # @dataclass()
-    # class OperationUnits(): # HACK 暂时不继续
-    #     """
-    #     定义运作单元（机器件）之结构化数组的数据（PyTorch 版本）
-    #     """
-    #
-    #     uid: torch.Tensor  # 运作单元之 ID
-    #     units_name: torch.Tensor  # 运作单元之名称
-    #     units_type: torch.Tensor  # 运作单元之类型
-    #     input_units: torch.Tensor  # 运作单元之输入
-    #     output_units: torch.Tensor  # 运作单元之输出
-    #     links: torch.Tensor  # 运作单元之连接（N×M）
-    #
-    #     def __init__(self, N_units: torch.int64, max_N_links: torch.int32, unit_type: torch.uint8):
-    #         self.uid = torch.arange(N_units, dtype=torch.int64)
-    #         self.units_name = torch.from_numpy(np.array([Tools.generate_unique_identifier() for i in range(N_units)]))
-    #         self.units_type = torch.from_numpy(np.array(Tools.encode_string_array(unit_type)))
-    #         self.input_units = torch.empty((N_units), dtype=torch.float32)
-    #         self.output_units = torch.empty((N_units), dtype=torch.float32)
-    #         self.links = torch.ones((N_units, max_N_links), dtype=torch.int32) * -1
-    #         pass  # function
-    #
-    #     pass  # class
+    @dataclass()
+    class OperationUnits():
+        """
+        定义运作单元（机器件）之结构化数组的数据
+
+        PyTorch 版本
+        """
+
+        def __init__(self, N_units: torch.uint64, N_char: torch.uint64, unit_type: torch.uint8, init_gid: torch.uint64):
+            """
+            初始化运作单元（机器件）之结构化数组的数据
+
+            Args:
+                N_units: 单元容量
+                N_char: 字符容量
+                unit_type: 单元类型
+                init_gid: 初始全局 ID 偏移值
+            """
+            self.gid = torch.arange(init_gid, init_gid + N_units)  # 运作单元之全局 ID
+            self.uid = torch.arange(N_units, dtype=torch.int64)  # 运作单元之 ID
+            self.state_on = torch.full(N_units, False)  # 运作单元在物理层面上是否被启用，True 表示启用，False 表示未启用
+            self.units_name = torch.from_numpy(np.array([Tools.generate_unique_identifier() for i in range(N_units)]))  # 运作单元之名称
+            self.units_type = torch.from_numpy(np.array(Tools.encode_string_array(unit_type)))  # 运作单元之类型
+            self.input_units = torch.empty((N_units, N_char), dtype=torch.uint32)  # 运作单元值输入
+            self.output_units = torch.empty((N_units, N_char), dtype=torch.uint32)  # 运作单元值输出
+            self.links_soft = torch.tensor((N_units, N_units), dtype=torch.uint64).to_sparse()  # 运作单元之 id 软连接（N×N COO 存储格式稀疏矩阵）
+            self.links_id = torch.tensor((N_units, N_units), dtype=torch.uint64).to_sparse()  # 运作单元之 id 硬连接（N×N COO 存储格式稀疏矩阵）
+            self.content = torch.empty((N_units, N_char), dtype=torch.uint32)  # 运作单元之内容
+            pass  # function
+
+        pass  # class
+
+    @dataclass()
+    class OperationUnitsForHuman():
+        """
+        定义可用于人类观察可读的运作单元（机器件）之结构化数组的数据
+
+        PyTorch 版本
+        """
+
+        def __init__(self, N_units: torch.uint64, N_char_explanation: torch.uint64, N_char_notes: torch.uint64, init_gid: torch.uint64):
+            """
+            初始化可用于人类观察可读的运作单元（机器件）之结构化数组的数据
+
+            Args:
+                N_units: 单元数量
+                N_char_explanation: 用于解释的字符容量
+                N_char_notes: 用于备注的字符容量
+                init_gid: 初始全局 ID 偏移值
+            """
+            self.gid = torch.arange(init_gid, init_gid + N_units)  # 运作单元之全局 ID
+            self.explanation = torch.empty((N_units, N_char_explanation), dtype=torch.uint32)  # 运作单元之解释
+            self.notes = torch.empty((N_units, N_char_notes), dtype=torch.uint32)  # 运作单元之备注
+            pass  # function
+
+        pass  # class
 
     @dataclass()
     class OperationUnits():
         """
-        定义运作单元（机器件）之结构化数组的数据（Numpy 版本）
+        定义运作单元（机器件）之结构化数组的数据
+
+        Numpy 版本
 
         注意，连接的数据类型为 int32，因为连接的值可能为负数。值为 -1 表示未连接。
         """
 
-        def __init__(self, N_units: np.uint32, max_N_links: np.uint32, unit_type: np.uint8, init_gid: np.uint32):
+        def __init__(self, N_units: np.uint64, max_N_links: np.uint64, unit_type: np.uint8, init_gid: np.uint64):
+            """
+            初始化运作单元（机器件）之结构化数组的数据
+
+            Args:
+                N_units: 运作单元容量
+                max_N_links: 运作单元之最大连接数
+                unit_type: 运作单元之类型
+                init_gid: 初始全局 ID 偏移值
+            """
             self.gid = np.arange(init_gid, init_gid + N_units)  # 单元之全局 ID
             self.uid = np.arange(N_units)  # 单元之 ID
             self.state_on = np.full(N_units, False)  # 运作单元在物理层面上是否被启用，True 表示启用，False 表示未启用
-            self.uid_name = np.array([Tools.generate_unique_identifier() for i in range(N_units)])  # 运作单元之名称
+            self.units_name = np.array([Tools.generate_unique_identifier() for i in range(N_units)])  # 运作单元之唯一名称
             self.units_type = np.full(N_units, unit_type)  # 运作单元之类型
             self.input_units = np.full(N_units, ' ', np.dtype('S128'))  # 运作单元之输入
             self.output_units = np.full(N_units, ' ', np.dtype('S128'))  # 运作单元之输出
-            self.links_softs = -np.ones((N_units, max_N_links), np.dtype(np.int32))  # 运作单元之软连接（N×M）
+            self.links_soft = -np.ones((N_units, max_N_links), np.dtype(np.int32))  # 运作单元之软连接（N×M）
             self.links_id = -np.ones((N_units, max_N_links), np.dtype(np.int32))  # 运作单元之 id 硬连接（N×M）
             self.content = np.full(N_units, ' ', np.dtype('S128'))  # 运作单元之内容
 
@@ -207,14 +253,25 @@ class ModelDefine():
     @dataclass()
     class OperationUnitsForHuman():
         """
-        定义可用于人类观察可读的运作单元（机器件）之结构化数组的数据（Numpy 版本）
+        定义可用于人类观察可读的运作单元（机器件）之结构化数组的数据
+
+        Numpy 版本
 
         注意，连接的数据类型为 int32，因为连接的值可能为负数。值为 -1 表示未连接。
         """
 
         def __init__(self, N_units: np.uint32, max_N_links: np.uint32, unit_type: np.uint8, init_gid: np.uint32):
+            """
+            初始化可用于人类观察可读的运作单元（机器件）之结构化数组的数据
+
+            Args:
+                N_units: 运作单元容量
+                max_N_links: 运作单元之最大连接数
+                unit_type: 运作单元之类型
+                init_gid: 初始全局 ID 偏移值
+            """
             self.gid = np.arange(init_gid, init_gid + N_units)  # 单元之全局 ID
-            self.explaination = np.full(N_units, ' ', np.dtype('S128'))  # 运作单元之解释
+            self.explanation = np.full(N_units, ' ', np.dtype('S4096'))  # 运作单元之解释
             self.notes = np.full(N_units, ' ', np.dtype('S4096'))  # 运作单元之备注
             pass  # function
 
